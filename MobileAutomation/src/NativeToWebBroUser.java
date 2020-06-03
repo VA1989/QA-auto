740e1b78616f3d8bfd7acadf2b28416c22d392de
VadimQA <abramenko_vadim@hotmail.com>
2020-06-03 18:40:47 +0300

    BroUser assert

---
+++ MobileAutomation/src/NativeToWebBroUser.java
@@ -0,0 +1,53 @@
+import java.net.MalformedURLException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
+import io.appium.java_client.android.AndroidDriver;
+
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+
+
+public class NativeToWebBroUser extends baseBroUser{
+
+
+
+public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+AndroidDriver<AndroidElement> driver=Capabilities("emulator");
+
+/*Set<String> context = driver.getContextHandles();
+
+for(String contextname :context) {
+	System.out.println(contextname);
+}*/
+
+ driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+ driver.get("https://bing.com");
+ driver.findElementByXPath("//*[@id=\"sb_form_q\"]").click();
+ driver.findElement(By.name("q")).sendKeys("https://wikipedia.org");
+ driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+ driver.findElementByXPath("//*[@text='Wikipedia https://www.wikipedia.org']").click();
+//*[@id="b_results"]/li[1]/div[1]/h2/a
+ String URL = driver.getCurrentUrl();
+ System.out.println (driver.getCurrentUrl());
+ Assert.assertEquals(URL, "https://wikipedia.org/" );
+ 
+}
+}
+
