6d810e2e394bb032741b391ebde809caae175020
VadimQA <abramenko_vadim@hotmail.com>
2020-06-03 20:18:35 +0300

    assert attempt 2

---
+++ MobileAutomation/src/NativeToWebBroUser2.java
@@ -0,0 +1,66 @@
+import java.net.MalformedURLException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
+import io.appium.java_client.android.AndroidDriver;
+
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+
+
+public class NativeToWebBroUser2 extends baseChrome{
+
+
+
+public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+AndroidDriver<AndroidElement> driver=Capabilities("emulator");
+
+/*Set<String> context = driver.getContextHandles();
+
+for(String contextname :context) {
+	System.out.println(contextname);
+}*/
+
+ driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+ driver.get("https://bing.com");
+ driver.findElementByXPath("//*[@id=\"sb_form_q\"]").click();
+ driver.findElement(By.name("q")).sendKeys("https://wikipedia.org");
+ driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+ //driver.findElementByClassName("android.widget.FrameLayout").click();
+ 
+ WebDriverWait wait = new WebDriverWait(driver, 10);
+
+//Find frame or iframe and switch
+wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.className("android.widget.FrameLayout")));
+
+//Now find the element 
+WebElement Category_Body = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Wikipedia https://www.wikipedia.org")));
+Category_Body.click();
+
+ //driver.findElementByXPath("//*[@id=\"b_results\"]/li[1]/div[1]/h2/a").click();
+//*[@id="b_results"]/li[1]/div[1]/h2/a
+ String URL = driver.getCurrentUrl();
+ System.out.println (driver.getCurrentUrl());
+ Assert.assertEquals(URL, "https://wikipedia.org/" );
+ 
+}
+}
+
